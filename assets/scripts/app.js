95f3593bf421592b445ae5a1efe39748768c9551
Mayank Sati <mmayanksati@gmail.com>
2021-06-28 15:05:32 +0530

    portfolio

---
+++ assets/scripts/app.js
@@ -0,0 +1,35 @@
+/* import Typed from 'typed.js'; */
+
+const Portfolio = function() {
+
+	function designForm() {
+		$("#my-modal form").addClass("my-form");
+	}
+
+	function typeAnimation() {
+		Typed.new("#writing-text", {
+			strings: [
+				"am a Full-Stack React Web Developer.", "I love building things.", "and. ", "solving problems."
+			],
+			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+			stringsElement: null,
+			// typing speed
+			typeSpeed: 1,
+			contentType: 'text',
+			callback: function() {
+				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
+			},
+			preStringTyped: function() {},
+			onStringTyped: function() {}
+		});
+	}
+
+	return {
+		typeAnimation: typeAnimation
+	}
+
+}();
+
+
+
+Portfolio.typeAnimation();
